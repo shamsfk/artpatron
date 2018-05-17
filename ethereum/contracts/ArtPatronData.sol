26b6340ced4074e8be8319620813aada93e77928
Bulat Shamsutdinov <bulat.shamsutdinov@gmail.com>
2018-05-17 17:31:12 +0300

    data commented

--- ethereum/contracts/ArtPatronData.sol
+++ ethereum/contracts/ArtPatronData.sol
@@ -1,53 +1,65 @@
 pragma solidity ^0.4.21;
 
-/// @title Art Patron Data constract
-/// Holds data of all Items, Authors and Holders
-/// @dev Data is stored in arrays
-/// @dev There is no write access in this contract
-/// @dev Read access
+/** @title Art Patron Data constract
+* @dev Holds data of all Items, Authors and Holders
+* @dev Data is stored in arrays
+* @dev There is no write access in this contract
+*/
 contract ArtPatronData {
     struct Item {
-        string name;
-        uint id;
-        uint authorId;
-        uint holderId;
-        uint creationDate;
-        uint currentBid;
-        uint marketDate;
-        address patronAddress;
+        string name;            // name of an item eg: "Mona Lisa"
+        uint id;                // index of an item in items array
+        uint authorId;          // index of an author in authors array
+        uint holderId;          // index of a holder in holders array
+        uint creationDate;      // date of item's creation
+        uint currentBid;        // price current patron payed
+        uint marketDate;        // date when item will be tradeable
+        address patronAddress;  // adress of current patron
     }
 
     struct Author {
-        string name;
-        uint id;
-        uint birthDate;
+        string name;    // author's name
+        uint id;        // index of an author in authors array
+        uint birthDate; // birth date of an author
     }
 
+    // Holder is an organisation that has Items like a museum or an art gallery
     struct Holder {
-        string name;
-        uint id;
-        uint16 countryId;
+        string name;        // holder's name
+        uint id;            // index of a holder in holders array
+        uint16 countryId;   // numerical index of a country holder resides in
     }
 
     Item[] internal items;
     Author[] internal authors;
     Holder[] internal holders;
 
-    mapping (uint => uint) internal itemToAuthor;
-    mapping (uint => uint) internal itemToHolder;
-
+    /**
+    * @dev Returns length of items array
+    */
     function GetItemsLength() external view returns (uint) {
         return items.length;
     }
 
+    /**
+    * @dev Returns length of authors array
+    */
     function GetAuthorsLength() external view returns(uint) {
         return authors.length;
     }
 
+    /**
+    * @dev Returns length of holders array
+    */
     function GetHoldersLength() external view returns(uint) {
         return holders.length;
     }
 
+    /**
+    * @dev Data of a single item
+    * @param _itemId Index of an item in items array
+    * @return Tuple with item's data
+    */
     function GetItemData(uint _itemId) external view
         returns (
             string name,
@@ -73,6 +85,11 @@ contract ArtPatronData {
         patronAddress = item.patronAddress;
     }
 
+    /**
+    * @dev Data of a single author
+    * @param _authorId Index of an author in authors array
+    * @return Tuple with author's data
+    */
     function GetAuthorData(uint _authorId) external view
         returns (
             string name,
@@ -88,6 +105,11 @@ contract ArtPatronData {
         birthDate = author.birthDate;
     }
 
+    /**
+    * @dev Data of a single item
+    * @param _holderId Index of an holder in holders array
+    * @return Tuple with holder's data
+    */
     function GetHolderData(uint _holderId) external view
         returns (
             string name,
